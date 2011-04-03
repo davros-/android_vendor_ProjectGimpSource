7bf14c4f97ddb40f29e14618e25ca70fbebe0d31
jason <gimpsource@gmail.com>
2011-04-03 15:07:46 -0400

    fixed common.mk

--- make-files/common.mk
+++ make-files/common.mk
@@ -17,9 +17,9 @@ PRODUCT_PACKAGES += \
     Superuser 
 
 # Copy changelog and credits to device
-PRODUCT_COPY_FILES += \
-    WE NEED TO TELL IT WHERE TO PULL YOUR FILES HERE  :system/etc/CHANGELOG-PGS.txt \
-    WE NEED TO TELL IT WHERE TO PULL YOUR FILES HERE  :system/etc/CREDITS-PGS.txt
+#PRODUCT_COPY_FILES += \
+    #WE NEED TO TELL IT WHERE TO PULL YOUR FILES HERE  :system/etc/CHANGELOG-PGS.txt \
+    #WE NEED TO TELL IT WHERE TO PULL YOUR FILES HERE  :system/etc/CREDITS-PGS.txt
 
 # Common Project GimpSource super-impose
-PRODUCT_COPY_FILES += \
+#PRODUCT_COPY_FILES += \
